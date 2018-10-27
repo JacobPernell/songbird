ad87cd828294af1ed78f364b27518e2140447de0
Mike Skalandunas <mskalandunas@gmail.com>
2018-10-26 22:13:49 -0700

    [26]: setup music endpoint

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const { ExpressServer } = require(__dirname + '/server/server');
 const { DIST_DIRECTORY } = require(__dirname + '/server/constants');
+const { ExpressServer } = require(__dirname + '/server/server');
+const { MusicRouter, UserRouter } = require(__dirname + '/router/index');
 
 const Server = new ExpressServer();
 
+Server.setupRouter('/api/', MusicRouter);
+Server.setupRouter('/', UserRouter);
 Server.setStaticDirectory(__dirname + '/' + DIST_DIRECTORY);
 Server.listen(process.env.PORT);
