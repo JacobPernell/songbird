edee59d38c76489b57eb64cbb9a843bd583e5103
Mike Skalandunas <mskalandunas@gmail.com>
2018-11-08 13:38:41 -0500

    [32 & 33]: add endpoint fixtures

---
+++ router/fixtures.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const PropTypes = require('prop-types');
+
+const { musicFixtures, userFixtures } = require('./constants');
+
+class Music {
+    constructor(id, sketches) {
+        this.id = id;
+        this.sketches = sketches;
+    }
+}
+
+Music.propTypes = {
+    id: PropTypes.number.isRequired,
+    sketches: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string,
+        notes: PropTypes.arrayOf(PropTypes.shape({
+            length: PropTypes.number.isRequired,
+            octave: PropTypes.number.isRequired,
+            pitch: PropTypes.string.isRequired
+        }))
+    }))
+};
+
+class User {
+    constructor({ email, id, username }) {
+        this.email = email;
+        this.id = id;
+        this.username = username;
+    }
+}
+
+User.propTypes = {
+    email: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    username: PropTypes.string
+};
+
+const createInstancesWithFixtures = (constructor, fixtures) =>
+    fixtures.map(fixture => new constructor({...fixture}));
+
+const musicEndpointFixtures = createInstancesWithFixtures(Music, musicFixtures);
+
+const userEndpointFixtures = createInstancesWithFixtures(User, userFixtures);
+
+module.exports = {
+    musicEndpointFixtures,
+    userEndpointFixtures
+};
